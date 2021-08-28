2634f58cc401319dd772152ea6f49799c7cbf4c2
Kinthamen <kinthamen@gmail.com>
2021-08-28 13:26:07 -0400

    Updated to use symantec versioning

--- bSpheres.py
+++ bSpheres.py
@@ -19,7 +19,7 @@ bl_info = {
     'name': 'bSpheres',
     'category': 'All',
     'author': 'Abinadi Cordova',
-    'version': (0, 0, 3),
+    'version': (1, 0, 0),
     'blender': (2, 93, 3),
     'location': '3D_Viewport window -> N-Panel > bSpheres',
     'description': 'bSpheres'
